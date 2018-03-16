763434ba125c69cc55542afb01e0ba7fcdbcd422
proton <whitedew.kilau@gmail.com>
2018-03-16 18:57:17 +0900

    second day commit

--- main.rb
+++ main.rb
@@ -1,12 +1,20 @@
 require 'sinatra'
 require 'sqlite3'
 require 'securerandom'
-
+require 'sinatra/reloader'
 db = SQLite3::Database.new 'db/hakase.db'
 db.results_as_hash = true
 
+get '/test' do
+  "Hello World!!"
+end
+
 get '/' do
 	erb :index
+end
+
+post '/index_post' do
+	redirect 'log_search_form'
 end
 
 get '/log_input_form' do
@@ -39,8 +47,66 @@ get '/log_search_form' do
 end
 
 post '/log_search_form_post' do
-	#search_key = params['usr_id']
-	readlog = db.execute('SELECT * FROM readlog')
-	p readlog[0][0].to_s
-	##p db.execute('SELECT * FROM readlog WHERE usr_id="' + search_key + '"')
+	search_key = params['usr_id']
+	#readlog = db.execute('SELECT * FROM readlog')
+	#p readlog[0][0].to_s 
+	result = db.execute('SELECT * FROM readlog WHERE usr_id="' + search_key + '"')
+	locals = {
+		search_key: search_key,
+		result: result
+	}
+	erb :log_search_form_post, :locals => locals
+end
+
+get '/grp_usr_check' do
+	locals = {
+		grp_usr: db.execute('SELECT * FROM grp_usr')
+	}
+	erb :grp_usr_check, :locals => locals
+end
+
+get '/grp_input_form' do
+	grp_usr = db.execute('SELECT * FROM grp_usr')
+	locals = {	
+		grp_usr: grp_usr
+	}
+	erb :grp_input_form, :locals => locals
+end
+
+post '/grp_input_form_post' do
+	grp_id = params['grp_id']
+	usr_id = params['usr_id']
+	db.execute('INSERT INTO grp_usr(grp_id, usr_id) VALUES("' + grp_id + '", "' + usr_id + '")')
+
+	redirect '/grp_input_form'
+end
+
+#search all group that a person joins
+get '/grp_search_form' do
+	erb :grp_search_form
+end
+
+post '/grp_search_form_post' do
+	search_key = params['usr_id']
+	result = db.execute('SELECT * FROM grp_usr WHERE usr_id="' + search_key + '"')
+	locals = {
+		search_key: search_key,
+		result: result
+	}
+	erb :grp_search_form_post, :locals => locals
+end
+
+get '/grp_tl' do
+	grp_id = params['grp_id']
+	#grp_id = "sikilab"
+	#grp_usr = db.execute('SELECT * FROM grp_usr WHERE grp_id="' + grp_id + '"')
+	
+	readlog = db.execute('SELECT * FROM readlog WHERE usr_id IN (
+						 SELECT usr_id FROM grp_usr WHERE grp_id="' + grp_id + '"
+						)')
+	locals = {
+		#grp_usr: grp_usr,
+		readlog: readlog
+	}
+	erb :grp_tl, :locals => locals
 end
--- src/db_create.rb
+++
@@ -1,5 +0,0 @@
-require 'sqlite3'
-
-db = SQLite3::Database.new '../db/hakase.db'
-
-db.execute('CREATE TABLE readlog(id INTEGER PRIMARY KEY, usr_id TEXT, pp_url TEXT, pp_title TEXT)')
